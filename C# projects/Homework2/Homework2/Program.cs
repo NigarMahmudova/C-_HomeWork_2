8e856f3b9001a6a2c29e72fd042e4cac4f3cf5d0
NigarMahmudova <nigarem@code.edu.az>
2023-01-17 11:23:37 -0800

    First commit

---
+++ C# projects/Homework2/Homework2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Homework2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task1
+            //Verilmiş N tam ədədinin rəqəmləri cəmini tapan proqram
+            Console.WriteLine("Ededi daxil edin...");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int result = 0;
+            while (N>0)
+            {
+                result += N % 10;
+                N = (N - N % 10) / 10;
+            }
+            Console.WriteLine("Netice..." + result);
+            #endregion
+
+            #region Task2
+            //Verilmiş M dəyərindən verilmiş K dəyərinədək 21-ə bölünən ədədlərin ədədi ortasını tapan proqram
+            Console.WriteLine("Birinci ededi daxil edin...");
+            int M = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ikinci ededi daxil edin...");
+            int K = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            int count = 0;
+            for (int i = M; i < K; i++)
+            {
+                if (i % 21 == 0)
+                {
+                    sum += i;
+                    count++;
+                }
+            }
+            if (count==0)
+            {
+                Console.WriteLine("21-e bolunen eded yoxdur.");
+            }
+            else
+            {
+                Console.WriteLine("21-e bolunen ededlerin ededi ortasi..." + sum/count);
+            }
+            #endregion
+
+            #region Task3
+            //Verilmis yazıda a herfi olub olmadigini tapan metod
+            Console.WriteLine("Yazi daxil edin...");
+            string word = Console.ReadLine();   
+            bool hasWantedChar = false;
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] == 'a')
+                {
+                    hasWantedChar = true;
+                    break;
+                }
+            }
+            if (hasWantedChar == true)
+            {
+                Console.WriteLine("Yazida a herfi var.");
+            }
+            else
+            {
+                Console.WriteLine("Yazida a herfi yoxdur.");
+            }
+            #endregion
+        }
+    }
+}
---
+++ C# projects/Homework2_method/Homework2_method/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace Homework2_method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task1
+            Console.WriteLine("Ededi daxil edin...");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int result = DigitsOfSum(N);
+            Console.WriteLine("Netice..." + result);
+            //Task2
+            Console.WriteLine("Birinci ededi daxil edin...");
+            int M = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ikinci ededi daxil edin...");
+            int K = Convert.ToInt32(Console.ReadLine());
+            int avg = AvgOfNumbers(M, K);
+            if (avg==0)
+            {
+                Console.WriteLine("21-e bolunen eded yoxdur.");
+            }
+            else 
+            {
+                Console.WriteLine("21-e bolunen ededlerin ededi ortasi..." + avg);
+            }
+            //Task3
+            Console.WriteLine("Yazi daxil edin...");
+            string word = Console.ReadLine();
+            string checkingChar = HasWantedChar(word);
+            Console.WriteLine(checkingChar);    
+        }
+        //Verilmiş N tam ədədinin rəqəmləri cəmini tapan metod 
+        static int DigitsOfSum (int N)
+        {
+            int result = 0;
+            while (N > 0)
+            {
+                result += N % 10;
+                N = (N - N % 10) / 10;
+            }
+            return result;
+        }
+        //Verilmiş M dəyərindən verilmiş N dəyərinədək 21-ə bölünən ədədlərin ədədi ortasını tapan metod
+        static int AvgOfNumbers (int M, int K) 
+        {
+            int sum = 0;
+            int count = 0;
+            int result = 0;
+            for (int i = M; i < K; i++)
+            {
+                if (i % 21 == 0)
+                {
+                    sum += i;
+                    count++;
+                }
+            }
+            if (count==0)
+            {
+                return 0;
+            }
+            else
+            {
+                result = sum / count;
+                return result;
+            }
+                
+            
+        }
+        //Verilmis yazıda a herfi olub olmadigini tapan metod
+        static string HasWantedChar(string word)
+        {
+            bool hasWantedA = false;
+            string yes = "Yazida a herfi var.";
+            string no = "Yazida a herfi yoxdur.";
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] == 'a')
+                {
+                    hasWantedA = true;
+                    break;
+                }
+            }
+            if (hasWantedA == true)
+            {
+                return yes;
+            }
+            else
+            {
+                return no;
+            }
+        }
+
+    }
+}
